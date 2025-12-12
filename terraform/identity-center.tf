d183e917f860ccb316bc8e2f8cdcf6f76afad57b
mitsu3s <mitsu3s.dev@gmail.com>
2025-12-12 22:50:20 +0900

    Add definition for identity center admin

--- terraform/identity-center.tf
+++ terraform/identity-center.tf
@@ -1,19 +1,41 @@
-# インスタンス情報の取得
+# 基本情報取得
 data "aws_ssoadmin_instances" "this" {}
+data "aws_caller_identity" "current" {}
+
+locals {
+  sso_instance_arn  = data.aws_ssoadmin_instances.this.arns[0]
+  identity_store_id = data.aws_ssoadmin_instances.this.identity_store_ids[0]
+}
+
+# Identity Group の Admins グループを定義
+resource "aws_identitystore_group" "admins" {
+  identity_store_id = local.identity_store_id
+  display_name      = "admins"
+  description       = "Administrators Group"
+}
 
 # Admin の Permission Set を定義
-resource "aws_ssoadmin_permission_set" "admin" {
-  name         = "Admin"
-  description  = "Administrator Access"
-  instance_arn = tolist(data.aws_ssoadmin_instances.this.arns)[0]
-
-  session_duration = "PT1H"
+resource "aws_ssoadmin_permission_set" "admins" {
+  name             = "Admins"
+  description      = "Administrators Access"
+  instance_arn     = local.sso_instance_arn
+  session_duration = "PT4H" # セッションの長さ
 }
 
 # Admin に アタッチするポリシーを定義
 # AWS 管理ポリシーの AdministratorAccess をアタッチ
-resource "aws_ssoadmin_managed_policy_attachment" "admin" {
-  instance_arn       = tolist(data.aws_ssoadmin_instances.this.arns)[0]
-  permission_set_arn = aws_ssoadmin_permission_set.admin.arn
+resource "aws_ssoadmin_managed_policy_attachment" "admins_access" {
+  instance_arn       = local.sso_instance_arn
+  permission_set_arn = aws_ssoadmin_permission_set.admins.arn
   managed_policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+# Admin グループに Permission Set をアサイン
+resource "aws_ssoadmin_account_assignment" "admins_to_account" {
+  instance_arn       = local.sso_instance_arn
+  permission_set_arn = aws_ssoadmin_permission_set.admins.arn
+  principal_type     = "GROUP"
+  principal_id       = aws_identitystore_group.admins.group_id
+  target_type        = "AWS_ACCOUNT"
+  target_id          = data.aws_caller_identity.current.account_id
 }
