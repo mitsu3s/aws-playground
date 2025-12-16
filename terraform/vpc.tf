64b83480d296329eabaae3d56bf866e58b411b66
mitsu3s <mitsu3s.dev@gmail.com>
2025-12-16 19:29:07 +0900

    Add two subnet for vpc

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,3 +1,13 @@
+### 利用可能なゾーンの取得
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+### 先頭から2つの利用可能なゾーンをローカル変数に格納
+locals {
+  availability_zones = slice(data.aws_availability_zones.available.names, 0, 2)
+}
+
 ### AWS Playground 用の VPC 作成
 resource "aws_vpc" "aws_playground_vpc" {
   cidr_block           = var.vpc_cidr
@@ -9,5 +19,23 @@ resource "aws_vpc" "aws_playground_vpc" {
     Project     = var.project
     Environment = var.environment
     ManagedBy   = "terraform"
+  }
+}
+
+### AWS Playground 用のパブリックサブネットを2つ作成
+### 1つ目の subnet は 1つ目の availability zone に、2つ目の subnet は 2つ目の availability zone に配置
+resource "aws_subnet" "aws_playground_public_subnet" {
+  count                   = length(local.availability_zones)
+  vpc_id                  = aws_vpc.aws_playground_vpc.id
+  availability_zone       = local.availability_zones[count.index]
+  cidr_block              = cidrsubnet(var.vpc_cidr, 8, count.index) # /16+8 = /24 からサブネットを分割
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name        = "${var.project}-${var.environment}-public-${count.index}"
+    Project     = var.project
+    Environment = var.environment
+    ManagedBy   = "terraform"
+    Tier        = "public"
   }
 }
