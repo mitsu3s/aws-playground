1eb0b32260bd61181551ff4845120cd779562447
mitsu3s <mitsu3s.dev@gmail.com>
2025-12-18 22:28:01 +0900

    Change to split security groups

---
+++ terraform/security-group.tf
@@ -0,0 +1,79 @@
+### AWS Playground 用のセキュリティグループ作成 (入口用)
+resource "aws_security_group" "aws_playground_ingress_security_group" {
+  name        = "${var.project}-${var.environment}-ingress"
+  description = "Ingress security group"
+  vpc_id      = aws_vpc.aws_playground_vpc.id
+
+  ### 後でルールを追加するので、ここでは空にしておく
+  # ingress {}
+
+  ### outbound はまず全許可にしておく
+  ### 外部から何かしら取得するなど、運用において必要な場合があるため
+  egress {
+    description = "All outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # 全てのプロトコル
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "${var.project}-${var.environment}-ingress"
+    Project     = var.project
+    Environment = var.environment
+    ManagedBy   = "terraform"
+    Role        = "ingress"
+  }
+}
+
+### AWS Playground 用のセキュリティグループ作成 (アプリケーション用)
+resource "aws_security_group" "aws_playground_app_security_group" {
+  name        = "${var.project}-${var.environment}-app"
+  description = "Application security group"
+  vpc_id      = aws_vpc.aws_playground_vpc.id
+
+  ### 後でルールを追加するので、ここでは空にしておく
+  # ingress {}
+
+  egress {
+    description = "All outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "${var.project}-${var.environment}-app"
+    Project     = var.project
+    Environment = var.environment
+    ManagedBy   = "terraform"
+    Role        = "app"
+  }
+}
+
+### AWS Playground 用のセキュリティグループ作成 (データベース用)
+resource "aws_security_group" "aws_playground_db_security_group" {
+  name        = "${var.project}-${var.environment}-db"
+  description = "Database security group"
+  vpc_id      = aws_vpc.aws_playground_vpc.id
+
+  ### 後でルールを追加するので、ここでは空にしておく
+  # ingress {}
+
+  egress {
+    description = "All outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "${var.project}-${var.environment}-db"
+    Project     = var.project
+    Environment = var.environment
+    ManagedBy   = "terraform"
+    Role        = "db"
+  }
+}
